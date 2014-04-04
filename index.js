26de4618501234ad9c6a5bc0ba3d91adab4a4ec2
DevYah <yahiaelgamal@gmail.com>
2014-04-05 00:32:31 +0200

    Add jade

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 var port = 3700;
 
+// jade stuff
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
+// static resources
+app.use(express.static(__dirname + '/public'));
 app.get("/", function(req, res) {
-  res.send("It works!");
+  res.render('page');
+  //res.send("It works!");
 });
 
 app.listen(port);
