4a390bed02b77d19cc918b6687b7613a06add637
DevYah <yahiaelgamal@gmail.com>
2014-04-05 02:52:44 +0200

    Add name and enter to send

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.get("/", function(req, res) {
 var io = require('socket.io').listen(app.listen(port)); // realtime
 
 io.sockets.on('connection', function(socket){
-  socket.emit('message', {message: 'welcome to the chat'});
+  socket.emit('message', {message:  'welcome to the chat', name: 'SYSTEM' });
+
   socket.on('send', function(data) {
     io.sockets.emit('message', data);
   });
--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,42 @@
 window.onload = function() {
-    var messages = [];
-    var socket = io.connect('http://localhost:3700');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
- 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data.message);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
-            }
-            content.innerHTML = html;
-        } else {
-            console.log("There is a problem:", data);
+  var messages = [];
+  var socket = io.connect('http://localhost:3700');
+  var field = document.getElementById("field");
+  var sendButton = document.getElementById("send");
+  var content = document.getElementById("content");
+  var name = document.getElementById("name");
+
+  socket.on('message', function (data) {
+    if(data.message) {
+      messages.push(data);
+      var html = '';
+      var i;
+      for(i=0; i<messages.length; i++) {
+        html += messages[i].name + ": ";
+        html += messages[i].message + '<br />';
+      }
+      content.innerHTML = html;
+      content.scrollTop(content.scrollHeight);
+    } else {
+      console.log("There is a problem:", data);
+    }
+  });
+
+  sendButton.onclick = sendMessage = function() {
+    if (name.value === '') {
+      alert('Please enter your name');
+      return;
+    }
+    var text = field.value;
+    socket.emit('send', { message: text, name: name.value });
+    field.value = '';
+  };
+};
+
+$(document).ready(function() {
+    $("#field").keyup(function(e) {
+        if(e.keyCode == 13) {
+            sendMessage();
         }
     });
- 
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
-    };
-};
+});
