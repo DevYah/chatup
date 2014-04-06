7f0984a0563d05d4953c940d5b46097b9fc7c65f
DevYah <yahiaelgamal@gmail.com>
2014-04-06 16:21:35 +0200

    prune deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -3,13 +3,6 @@
 sudo apt-get -y install git
 sudo apt-get -y install nodejs
 
-
-sudo apt-get install python-software-properties
-sudo apt-add-repository ppa:chris-lea/node.js
-sudo apt-get update
-
-npm install -g forever
-
 #git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 git clone https://github.com/DevYah/chatup.git 
